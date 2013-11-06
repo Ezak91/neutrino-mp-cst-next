tuxbox-neutrino/neutrino

commit 404de5376b75c96bb3d79590b9cedc5cb8f8533e

gui/personalize.cpp: fix CPinSetupNotifier: return false, menu repaint not needed

src/gui/personalize.cpp
@@ -974,5 +974,6 @@ bool CPinSetupNotifier::changeNotify(const neutrino_locale_t, void *)
 {
 	toDisable->setActive(g_settings.personalize[SNeutrinoSettings::P_MAIN_PINSTATUS]);
    
-   return g_settings.personalize[SNeutrinoSettings::P_MAIN_PINSTATUS];
+//   return g_settings.personalize[SNeutrinoSettings::P_MAIN_PINSTATUS];
+	return false;
 }

Ezak91/neutrino-mp-cst-next

commit 25abecc6addf74ca0bfb37f69e5453f9ea226832

gui/personalize.cpp: fix pin change active/enactive, broken after commit 404de5376b75c96bb3d79590b9cedc5cb8f8533e

src/gui/personalize.cpp
@@ -378,7 +378,7 @@ void CPersonalizeGui::ShowPinSetup(CMenuWidget* p_widget, CPINChangeWidget * &pi
  	pin_setup_notifier = new CPinSetupNotifier(fw_pin_setup);
  	p_widget->addItem(new CMenuOptionChooser(LOCALE_PERSONALIZE_PIN_IN_USE, &g_settings.personalize[SNeutrinoSettings::P_MAIN_PINSTATUS], OPTIONS_OFF0_ON1_OPTIONS, OPTIONS_OFF0_ON1_OPTION_COUNT, true, pin_setup_notifier));
 	
-	fw_pin_setup->setActive(pin_setup_notifier->changeNotify());
+	pin_setup_notifier->changeNotify();
 	p_widget->addItem(fw_pin_setup);
 	
 	p_widget->addItem(new CMenuSeparator(CMenuSeparator::LINE | CMenuSeparator::STRING, LOCALE_PERSONALIZE_MENUCONFIGURATION));