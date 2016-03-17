tuxbox-neutrino/neutrino

commit 10c430d40e2b537882941c14f41f57724e4a87ba

cc_frm_scrollbar.cpp icon dont need full path

src/gui/components/cc_frm_scrollbar.cpp
@@ -81,8 +81,8 @@ void CComponentsScrollBar::initVarSbForm(const int& count)
 	sb_segments_obj = NULL;
 
 	string ftype = ".png";
-	sb_up_icon	= frameBuffer->getIconBasePath() + "/" + NEUTRINO_ICON_BUTTON_TOP + ftype;
-	sb_down_icon	= frameBuffer->getIconBasePath() + "/" + NEUTRINO_ICON_BUTTON_DOWN + ftype;
+	sb_up_icon	= NEUTRINO_ICON_BUTTON_TOP + ftype;
+	sb_down_icon	= NEUTRINO_ICON_BUTTON_DOWN + ftype;
 
 	sb_segments_count = count;
 	sb_mark_id	= 0;

Ezak91/neutrino-mp-cst-next

commit 46a0a720084adf50139ddfa7d1a4c5d1af525c22

CComponentsScrollBar: fix missed navi arrows
Paths were broken
comes in with 10c430d40e2b537882941c14f41f57724e4a87ba

src/gui/components/cc_frm_scrollbar.cpp
@@ -80,9 +80,8 @@ void CComponentsScrollBar::initVarSbForm(const int& count)
 	sb_up_obj 	= sb_down_obj = NULL;
 	sb_segments_obj = NULL;
 
-	string ftype = ".png";
-	sb_up_icon	= NEUTRINO_ICON_BUTTON_TOP + ftype;
-	sb_down_icon	= NEUTRINO_ICON_BUTTON_DOWN + ftype;
+	sb_up_icon	= frameBuffer->getIconPath(NEUTRINO_ICON_BUTTON_TOP) ;
+	sb_down_icon	= frameBuffer->getIconPath(NEUTRINO_ICON_BUTTON_DOWN);
 
 	sb_segments_count = count;
 	sb_mark_id	= 0;